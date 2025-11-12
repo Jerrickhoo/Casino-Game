2cf2bb112952fe1b46c6ecaffc13bb9b4dba8260
jasesino <ja.japitana@gmail.com>
2025-11-12 21:23:30 +0800

    jaja Lucky9

--- src/CasinoMain.java
+++ src/CasinoMain.java
@@ -2,6 +2,7 @@ import utilities.utilities;
 import Core.Player;
 import Core.PlayerDatabase;
 import games.SlotMachine.SlotMachine;
+import games.Lucky9.Lucky9;
 
 public class CasinoMain {
     private static PlayerDatabase playerDB;
@@ -289,7 +290,7 @@ public class CasinoMain {
 
             switch (choice) {
                 case 1:
-                    // playLucky9();
+                    Lucky9.play(currentPlayer, playerDB);
                     break;
                 case 2:
                     // playBlackjack();
